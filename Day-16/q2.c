e5223170f66786b689222417beae8a2044b409ed
Palak Sharma <29palaksharma@gmail.com>
2026-06-13 08:14:58 +0530

    Organized Day 16 assignment

---
+++ Day-16/q2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(){
+    int n, i, j, count, maxCount = 0, maxElement;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+    for(i = 0; i < n; i++){
+        count = 1;
+        for(j = i + 1; j < n; j++){
+            if(arr[i] == arr[j]){
+                count++;
+            }
+        }
+        if(count > maxCount){
+            maxCount = count;
+            maxElement = arr[i];
+        }
+    }
+    printf("Element with maximum frequency = %d", maxElement);
+    printf("\nFrequency = %d", maxCount);
+    return 0;
+}
