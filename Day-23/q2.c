977da288b43a7f7c07e370ce3ecf73165a41abbe
Palak Sharma <29palaksharma@gmail.com>
2026-06-19 23:08:14 +0530

    Organized Day 23 assignment

---
+++ Day-23/q2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    char str[100];
+    int i,j;
+    printf("Enter string: ");
+    fgets(str,sizeof(str),stdin);
+    for(i=0;str[i]!='\0';i++){
+        for(j=i+1;str[j]!='\0';j++){
+            if(str[i]==str[j]){
+                printf("First repeating character:%c",str[i]);
+                return 0;
+            }
+        }
+    }
+    printf("No repeating character found");
+    return 0;
+}
---
+++ Day-23/q4.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    char str[100];
+    int i,j,count;
+    int maxCount=0;
+    char maxChar;
+    printf("Enter a string:");
+    scanf("%s",str);
+    for(i=0;str[i]!='\0';i++){
+        count=0;
+        for(j=0;str[j]!='\0';j++){
+            if(str[i]==str[j]){
+                count++;
+            }
+        }
+        if(count>maxCount){
+            maxCount=count;
+            maxChar=str[i];
+        }
+    }
+    printf("Maximum occurring character: %c\n",maxChar);
+    printf("Frequency: %d",maxCount);
+    return 0;
+}
